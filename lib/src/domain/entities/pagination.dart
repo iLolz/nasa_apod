f764ae656e8fbfd3be26bba5fdae97363e71fdd9
Alencar <alencarantonio@hotmail.com.br>
2023-09-17 11:52:22 -0300

    feature: creta pagination entity

---
+++ lib/src/domain/entities/pagination.dart
@@ -0,0 +1,11 @@
+class Pagination {
+  final DateTime startDate;
+  final DateTime endDate;
+  final bool thumbs;
+
+  const Pagination({
+    required this.startDate,
+    required this.endDate,
+    required this.thumbs,
+  });
+}
