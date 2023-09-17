0bf80ac5e79f3535cd67f8c4eac4917e5ff7eb1a
Alencar <alencarantonio@hotmail.com.br>
2023-09-17 12:17:40 -0300

    feature: create pagination model

---
+++ lib/src/data/pagination_model.dart
@@ -0,0 +1,17 @@
+import '../domain/entities/pagination.dart';
+
+class PaginationModel extends Pagination {
+  const PaginationModel({
+    required super.startDate,
+    required super.endDate,
+    required super.thumbs,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'start_date': startDate,
+      'end_date': endDate,
+      'thumbs': thumbs,
+    };
+  }
+}
