dc56ee77fa8dfbf0a72b984a029919bee03bc604
Alencar <alencarantonio@hotmail.com.br>
2023-09-17 11:52:05 -0300

    feature: create image info entity

---
+++ lib/src/domain/entities/image_info.dart
@@ -0,0 +1,13 @@
+class ImageInfo {
+  final String imageUrl;
+  final String title;
+  final String description;
+  final DateTime date;
+
+  ImageInfo({
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.date,
+  });
+}
