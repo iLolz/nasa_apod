6321a84d183dbd9d35fbec0189bcab8f11acfcce
Alencar <alencarantonio@hotmail.com.br>
2023-09-17 12:15:11 -0300

    feature: create image info model

---
+++ lib/src/data/image_info_model.dart
@@ -0,0 +1,39 @@
+import '../domain/entities/image_info.dart';
+
+class ImageInfoModel {
+  final DateTime date;
+  final String explanation;
+  final String hdImageUrl;
+  final String title;
+  final String mediaType;
+  final String imageUrl;
+
+  const ImageInfoModel({
+    required this.date,
+    required this.explanation,
+    required this.hdImageUrl,
+    required this.title,
+    required this.mediaType,
+    required this.imageUrl,
+  });
+
+  factory ImageInfoModel.fromMap(Map<String, dynamic> json) {
+    return ImageInfoModel(
+      date: DateTime.parse(json['date']),
+      explanation: json['explanation'],
+      hdImageUrl: json['hdurl'],
+      title: json['title'],
+      mediaType: json['media_type'],
+      imageUrl: json['url'],
+    );
+  }
+
+  ImageInfo toEntity() {
+    return ImageInfo(
+      date: date,
+      description: explanation,
+      imageUrl: hdImageUrl,
+      title: title,
+    );
+  }
+}
