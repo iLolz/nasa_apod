b949c02b710fe0912ae7638a863344b7331df623
Alencar <alencarantonio@hotmail.com.br>
2023-09-19 22:39:29 -0300

    refactor: improve DioService

--- lib/src/core/services/dio_service.dart
+++ lib/src/core/services/dio_service.dart
@@ -1,21 +1,17 @@
 import 'package:dio/dio.dart';
 
 class DioService {
-  late String baseUrl;
-  late String apiKey;
-
   Dio setup() {
-    baseUrl = const String.fromEnvironment('baseUrl');
-    apiKey = const String.fromEnvironment('apiKey');
+    String baseUrl = const String.fromEnvironment('baseUrl');
+    String apiKey = const String.fromEnvironment('apiKey');
 
     final BaseOptions options = BaseOptions(
-      baseUrl: baseUrl,
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 3),
-      queryParameters: {
-        'api_key': apiKey,
-      },
-    );
+        baseUrl: baseUrl,
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 3),
+        queryParameters: {
+          'api_key': apiKey,
+        });
 
     return Dio(options);
   }
