eb9eb734b2c507db3479a2fe7f59ceadb54ec2e0
Alencar <alencarantonio@hotmail.com.br>
2023-09-19 22:46:32 -0300

    feature: add ImagesList widget

---
+++ lib/src/presentation/home/widgets/loaded/images_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:nasa_apod/src/presentation/home/widgets/loaded/image_card.dart';
+
+import '../../../../domain/entities/image_info.dart';
+
+class ImagesList extends StatelessWidget {
+  final List<ApodImage> images;
+
+  const ImagesList(
+    this.images, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      for (ApodImage image in images) ...[
+        ImageCard(image: image),
+        const SizedBox(
+          height: 8.0,
+        ),
+      ],
+    ]);
+  }
+}
